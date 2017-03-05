c050a8afefc65ad29518a2f72628ece36f2dd3c3
liududu <3037565218@qq.com>
2017-03-05 11:00:40 +0800

    use TP framework

---
+++ Application/Home/Controller/TestController.class.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: liududu
+ * Date: 17-3-4
+ * Time: 下午12:34
+ */
+namespace Home\Controller;
+use Think\Controller;
+
+class testController extends Controller{
+    public function test(){
+        echo "hello,world";
+    }
+}
